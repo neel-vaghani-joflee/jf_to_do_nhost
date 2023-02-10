dea95654b75fca548ab1fab0926fc9bd9d4fc408
Neel Vaghani <neel.vaghani@joflee.com>
2023-02-10 10:49:45 +0530

    [ Working ] Working on auth

---
+++ lib/core/nhost_string_const.dart
@@ -0,0 +1,6 @@
+// ignore_for_file: constant_identifier_names
+
+abstract class NhostStringConst {
+  static const CLIENT_URL = 'http://localhost:3000';
+  static const NHOST_BACKEND_URL = 'https://jxldkdsrnravjzkwttmv.nhost.run';
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,212 @@
+// Demonstrates sign in, logout, and requiring authentication to access a
+/// resource.
+library simple_auth_example;
+
 import 'package:flutter/material.dart';
+import 'package:nhost_flutter_auth/nhost_flutter_auth.dart';
+
+import 'core/nhost_string_const.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(SimpleAuthExample());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class SimpleAuthExample extends StatefulWidget {
+  @override
+  _SimpleAuthExampleState createState() => _SimpleAuthExampleState();
+}
 
-  // This widget is the root of your application.
+class _SimpleAuthExampleState extends State<SimpleAuthExample> {
+  late NhostClient nhostClient;
+
+  @override
+  void initState() {
+    super.initState();
+    // Create a new Nhost client using your project's backend URL.
+    nhostClient = NhostClient(backendUrl: NhostStringConst.NHOST_BACKEND_URL);
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    nhostClient.close();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    // The NhostAuthProvider widget provides authentication state to its
+    // subtree, which can be accessed using NhostAuthProvider.of(BuildContext).
+    return NhostAuthProvider(
+      auth: nhostClient.auth,
+      child: MaterialApp(
+        title: 'Nhost.io Simple Flutter Authentication',
+        home: Scaffold(
+          body: ExampleProtectedScreen(),
+        ),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class ExampleProtectedScreen extends StatelessWidget {
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  Widget build(BuildContext context) {
+    // NhostAuthProvider.of will register this widget so that it rebuilds whenever
+    // the user's authentication state changes.
+    final auth = NhostAuthProvider.of(context)!;
+    Widget widget;
+    switch (auth.authenticationState) {
+      case AuthenticationState.signedIn:
+        widget = LoggedInUserDetails();
+        break;
+      case AuthenticationState.signedOut:
+        widget = SignInForm();
+        break;
+      default:
+        widget = SizedBox();
+        break;
+    }
+
+    return Padding(
+      padding: EdgeInsets.all(32),
+      child: widget,
+    );
+  }
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+const rowSpacing = SizedBox(height: 12);
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+class SignInForm extends StatefulWidget {
+  @override
+  _SignInFormState createState() => _SignInFormState();
+}
+
+class _SignInFormState extends State<SignInForm> {
+  final formKey = GlobalKey<FormState>();
+  late TextEditingController emailController;
+  late TextEditingController passwordController;
+
+  @override
+  void initState() {
+    super.initState();
+    emailController = TextEditingController(text: 'user-1@nhost.io');
+    passwordController = TextEditingController(text: 'password-1');
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    emailController.dispose();
+    passwordController.dispose();
+  }
+
+  void trySignIn() async {
+    final auth = NhostAuthProvider.of(context)!;
+
+    try {
+      await auth.signInEmailPassword(
+          email: emailController.text, password: passwordController.text);
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          backgroundColor: Colors.green,
+          content: Text('Done'),
+        ),
+      );
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text(e.toString()),
+        ),
+      );
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+    return Form(
+      key: formKey,
+      child: FocusTraversalGroup(
+        policy: ReadingOrderTraversalPolicy(),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            TextFormField(
+              controller: emailController,
+              decoration: InputDecoration(
+                hintText: 'Email',
+                border: OutlineInputBorder(),
+              ),
+              onFieldSubmitted: (_) => trySignIn(),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+            rowSpacing,
+            TextFormField(
+              controller: passwordController,
+              decoration: InputDecoration(
+                hintText: 'Password',
+                border: OutlineInputBorder(),
+              ),
+              obscureText: true,
+              onFieldSubmitted: (_) => trySignIn(),
             ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: trySignIn,
+              child: Text('Submit'),
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
+
+class LoggedInUserDetails extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final auth = NhostAuthProvider.of(context)!;
+    final currentUser = auth.currentUser!;
+
+    final textTheme = Theme.of(context).textTheme;
+    const cellPadding = EdgeInsets.all(4);
+
+    return Center(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Welcome ${currentUser.email}!',
+            style: textTheme.headline5,
+          ),
+          rowSpacing,
+          Text('User details:', style: textTheme.caption),
+          rowSpacing,
+          Table(
+            defaultColumnWidth: IntrinsicColumnWidth(),
+            children: [
+              for (final row in currentUser.toJson().entries)
+                TableRow(
+                  children: [
+                    Padding(
+                      padding: cellPadding.copyWith(right: 12),
+                      child: Text(row.key),
+                    ),
+                    Padding(
+                      padding: cellPadding,
+                      child: Text('${row.value}'),
+                    ),
+                  ],
+                )
+            ],
+          ),
+          rowSpacing,
+          ElevatedButton(
+            onPressed: () {
+              auth.signOut();
+            },
+            child: Text('Logout'),
+          ),
+        ],
+      ),
     );
   }
 }
